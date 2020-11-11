9c10f75b03c3280bce0e5dc66de265cc03019132
Max <max@dfinity.org>
2020-11-11 23:59:48 +0100

    Allow single character variable names

--- rust/lib.rs
+++ rust/lib.rs
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#![allow(clippy::many_single_char_names)]
 pub mod arch;
 pub mod aes;
 pub mod gcm;
